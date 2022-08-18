459706b1dbb89adbfb5d82e56366f5376f281776
Lucas Soares <lsoares@ibm.com>
2022-08-18 17:07:39 -0300

    compatibility fixes

--- client/src/components/DatePicker/index.js
+++ client/src/components/DatePicker/index.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 
 import TextField from "@mui/material/TextField";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { AdapterDateFns } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { DesktopDatePicker } from "@mui/x-date-pickers";
 
 export default function DatePicker({ value, setValue, date, label }) {
   const handleChange = (newValue) => {
@@ -14,7 +14,7 @@ export default function DatePicker({ value, setValue, date, label }) {
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider>
       <DesktopDatePicker
         label={label}
         inputFormat="dd/MM/yyyy"
